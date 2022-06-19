756792ff627c7c3f2aacca12032d089b3bd0ae73
Frederic <bemeraude@hotmail.com>
2022-06-19 10:45:47 -0400

    auto commit

--- process_data.py
+++ process_data.py
@@ -16,15 +16,6 @@ def get_dataFrame(argdict):
 
 
     try:
-<<<<<<< HEAD
-        dfTrain=pd.read_csv(f"{argdict['pathFolder']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/{argdict['labelled_dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t', index_col=0)
-    except:
-        create_train=True
-    os.makedirs(f"{argdict['pathFolder']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/{argdict['labelled_dataset_size']}",exist_ok=True)
-
-    if create_train:
-        dfTrain=pd.read_csv(f"{argdict['pathData']}/Dataset/{task}/train.tsv", sep='\t')
-=======
         dfTrain=pd.read_csv(f"{argdict['path']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t', index_col=0)
     except:
         create_train=True
@@ -32,38 +23,10 @@ def get_dataFrame(argdict):
 
     if create_train:
         dfTrain=pd.read_csv(f"{argdict['path']}/data/{task}/train.tsv", sep='\t')
->>>>>>> f6f8d189e49765d8885a8565af4bb994174e3c3c
 
 
         # print(len(dfTrain))
         # fds
-<<<<<<< HEAD
-    dfVal=pd.read_csv(f'{argdict["pathData"]}/Dataset/{task}/dev.tsv', sep='\t')
-    dfTest=pd.read_csv(f'{argdict["pathData"]}/Dataset/{task}/test.tsv', sep='\t')
-
-    if create_train:
-        if argdict['labelled_dataset_size']!=0:
-            #Sampling balanced data
-            # print(len(dfTrain[dfTrain['label']==0]))
-            # prop=len(dfTrain[dfTrain['label']==0])/len(dfTrain)
-            dfTrain['true_label']=dfTrain['label']
-            nb_points=math.ceil(argdict['labelled_dataset_size']/len(argdict['categories']))
-            # print(prop)
-            # print(int(argdict['dataset_size']/len(argdict['categories'])))
-            # print(argdict['labelled_dataset_size'])
-            NewdfTrain=dfTrain[dfTrain['label']==0].sample(n=nb_points)
-            for i in range(1, len(argdict['categories'])):
-                # print(int(argdict['dataset_size']/len(argdict['categories'])))
-                # print(i)
-                NewdfTrain=pd.concat([NewdfTrain ,dfTrain[dfTrain['label']==i].sample(n=nb_points)])
-            dfTrain.drop(NewdfTrain.index)
-            dfTrain = dfTrain.assign(label=len(argdict['categories']))
-            dfTrain=pd.concat([NewdfTrain, dfTrain])
-            dfTrain.to_csv(f"{argdict['pathFolder']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/{argdict['labelled_dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t')
-        else:
-            dfTrain['true_label'] = dfTrain['label']
-            dfTrain.to_csv(f"{argdict['pathFolder']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/{argdict['labelled_dataset_size']}/train_{argdict['random_seed']}.tsv",sep='\t')
-=======
     dfVal=pd.read_csv(f'{argdict["path"]}/data/{task}/dev.tsv', sep='\t')
     dfVal=pd.read_csv(f'{argdict["path"]}/data/{task}/dev.tsv', sep='\t')
     dfTest=pd.read_csv(f'{argdict["path"]}/data/{task}/test.tsv', sep='\t')
@@ -86,7 +49,6 @@ def get_dataFrame(argdict):
         dfTrain = dfTrain.assign(label=len(argdict['categories']))
         dfTrain=pd.concat([NewdfTrain, dfTrain])
         dfTrain.to_csv(f"{argdict['path']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t')
->>>>>>> f6f8d189e49765d8885a8565af4bb994174e3c3c
     return dfTrain, dfVal, dfTest
 
 def process_data(argdict):
@@ -120,39 +82,6 @@ def process_data(argdict):
     file.write(devLabel)
     file.close()
 
-<<<<<<< HEAD
-    # #Baseline
-    # olddsSize=argdict['labelled_dataset_size']
-    # argdict['labelled_dataset_size']=argdict['labelled_dataset_size']+argdict["batch_size_iter"]*argdict["nb_iter"]
-    # dfTrain, dfDev=get_dataFrame(argdict)
-    # train=""
-    # trainLabel=""
-    # dev=""
-    # devLabel=""
-    # for sent in dfTrain['sentence']:
-    #     train+=sent+"\n"
-    # for lab in dfTrain["label"]:
-    #     trainLabel+=str(lab)+"\n"
-    # for sent in dfDev['sentence']:
-    #     dev+=sent+"\n"
-    # for lab in dfDev["label"]:
-    #     devLabel+=str(lab)+"\n"
-    # os.makedirs(f"{argdict['pathFolder']}/Generators/{argdict['algo']}/Databaseline", exist_ok=True)
-    # file = open(f"{argdict['pathFolder']}/Generators/{argdict['algo']}/Databaseline/ptb.train.txt", "w")
-    # file.write(train)
-    # file.close()
-    # file = open(f"{argdict['pathFolder']}/Generators/{argdict['algo']}/Databaseline/ptb.train.label.txt", "w")
-    # file.write(trainLabel)
-    # file.close()
-    # file = open(f"{argdict['pathFolder']}/Generators/{argdict['algo']}/Databaseline/ptb.dev.txt", "w")
-    # file.write(dev)
-    # file.close()
-    # file = open(f"{argdict['pathFolder']}/Generators/{argdict['algo']}/Databaseline/ptb.dev.label.txt", "w")
-    # file.write(devLabel)
-    # file.close()
-    # argdict['labelled_dataset_size']=olddsSize
-=======
->>>>>>> f6f8d189e49765d8885a8565af4bb994174e3c3c
 
 def add_data(argdict, sentences, labels, datasetsLabelled):
     """Add labelled data to the algo"""
@@ -175,7 +104,6 @@ def dataLoaderToArray(dataloader):
         y.append(data['label'])
     return x, y
 
-<<<<<<< HEAD
 def compareFiles(argdict, file, folder):
     """Compare the argdict and the string of parameter EXCEPT for the computer parameter
     folder can be gen or exp, for generated or experiments"""
@@ -256,7 +184,4 @@ def log_results(argdict, results, i):
     # path = f"{argdict['pathDataAdd']}/GeneratedData/{argdict['algo']}/{argdict['dataset']}/{argdict['dataset_size']}/{argdict['numFolderGenerated']}"
     # with open(f"{path}/param.json", "w") as f:
     #     json.dump(argdict, f)
-=======
 
-
->>>>>>> f6f8d189e49765d8885a8565af4bb994174e3c3c
