594581eb23546427859d923b0ea62af8c821ebf0
Frederic <bemeraude@hotmail.com>
2022-06-21 15:27:06 -0400

    auto commit

--- Encoders/GRU.py
+++ Encoders/GRU.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class GRU_Encoder(nn.Module):
 
 	def __init__(self, argdict, vocab_size, embedding_size):
+		super().__init__()
 		self.argdict=argdict
 
 		self.embedding = nn.Embedding(vocab_size, embedding_size)
