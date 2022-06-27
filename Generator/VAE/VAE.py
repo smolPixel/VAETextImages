9ffbf760642b4ba527dc78228d10212943bb8ee1
Frederic <bemeraude@hotmail.com>
2022-06-27 14:23:59 -0400

    auto commit

--- Generator/VAE/VAE.py
+++ Generator/VAE/VAE.py
@@ -131,7 +131,7 @@ class VAE():
                 logp, mean, logv, z = self.model(batch)
 
                 print(logp.shape)
-                print(batch['target'].shape
+                print(batch['target'].shape)
 
                 # loss calculation
                 # NLL_loss, KL_loss, KL_weight = loss_fn(logp, batch['target'],
