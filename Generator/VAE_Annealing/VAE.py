b192fc8aba48b580ae9c347cf80547ee4df416f3
Frederic <bemeraude@hotmail.com>
2022-07-04 09:58:16 -0400

    auto commit

--- Generator/VAE_Annealing/VAE.py
+++ Generator/VAE_Annealing/VAE.py
@@ -92,7 +92,8 @@ class VAE_Annealing():
             for iteration, batch in enumerate(data_loader):
                 # Forward pass
                 logp, mean, logv, z = self.model(batch)
-                # batch_size=logp.shape[0]
+                batch_size=logp.shape[0]
+                print(batch_size)
                 logp, target=self.datasets['train'].shape_for_loss_function(logp, batch['target'])
                 # NLL_loss, KL_loss, KL_weight = loss_fn(logp, batch['target'],
                 #                                        batch['length'], mean, logv, self.argdict.anneal_function, step,
@@ -101,7 +102,7 @@ class VAE_Annealing():
                                                             0.0025)
                 # print(NLL_loss)
                 batch_size = logp.shape[0]
-                print(batch_size)
+                # print(batch_size)
                 loss = (NLL_loss + KL_weight * KL_loss) / batch_size
 
                 # backward + optimization
