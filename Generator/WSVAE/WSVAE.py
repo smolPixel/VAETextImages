9f28e33e5064b4c506a22b02152f0193b63553b8
Frederic <bemeraude@hotmail.com>
2022-07-21 14:44:30 -0400

    auto commit

--- Generator/WSVAE/WSVAE.py
+++ Generator/WSVAE/WSVAE.py
@@ -158,8 +158,8 @@ class WSVAE():
                     # print(output.shape)
                     # print(batch['label'])
                     loss=self.loss_function_discriminator(output, batch['label'].cuda())
-                    print(torch.argmax(torch.softmax(output, dim=-1).cpu().detach(), dim=-1))
-                    preds.extend(torch.argmax(torch.softmax(output, dim=-1).cpu().detach()))
+                    # print(torch.argmax(torch.softmax(output, dim=-1).cpu().detach(), dim=-1))
+                    preds.extend(torch.argmax(torch.softmax(output, dim=-1).cpu().detach(), dim=-1).tolist())
                     ground_truth.extend(batch['label'])
                     losses.append(loss.item())
                     if split == 'train':
