cb238a9c5967c13a6286dd90918f708520d76a32
Frederic <bemeraude@hotmail.com>
2022-07-21 14:06:49 -0400

    auto commit

--- Generator/VAE/model.py
+++ Generator/VAE/model.py
@@ -26,6 +26,8 @@ class VAE_model(nn.Module):
 
         z = to_var(torch.randn([batch_size, self.argdict['latent_size']]))
         z = z * std + mean
+        print(z.shape)
+        fds
         logp = self.decoder(input_sequence, z)
 
         return logp, mean, logv, z
