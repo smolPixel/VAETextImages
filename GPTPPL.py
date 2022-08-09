84a830bce3084ac414c2c80284e4c27e3062dc0b
Frederic <bemeraude@hotmail.com>
2022-08-09 08:35:27 -0400

    auto commit

--- GPTPPL.py
+++ GPTPPL.py
@@ -1,7 +1,7 @@
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 
 #https://huggingface.co/docs/transformers/perplexity
-device = "cpu"
+device = "cuda:0"
 model_id = "gpt2-large"
 model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
 tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
