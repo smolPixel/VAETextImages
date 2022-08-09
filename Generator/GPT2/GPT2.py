2026ef440fb0fe28cfabe9001909cc2b7e6a9096
Frederic <bemeraude@hotmail.com>
2022-08-09 10:19:41 -0400

    auto commit

--- Generator/GPT2/GPT2.py
+++ Generator/GPT2/GPT2.py
@@ -4,16 +4,16 @@ import torch
 
 class GPT2():
 
-    def __init__(self, argdict, train, dev, test):
-        self.argdict=argdict
-        self.splits=['train', 'dev']
-        self.datasets={'train':train, 'dev':dev, 'test':test}
-        self.model, self.params=self.init_model_dataset()
-        # optimizers
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)  # self.argdict.learning_rate)
-        self.loss_function_basic=train.loss_function
+	def __init__(self, argdict, train, dev, test):
+		self.argdict=argdict
+		self.splits=['train', 'dev']
+		self.datasets={'train':train, 'dev':dev, 'test':test}
+		self.model, self.params=self.init_model_dataset()
+		# optimizers
+		self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)  # self.argdict.learning_rate)
+		self.loss_function_basic=train.loss_function
 
-    def init_model_dataset(self):
+	def init_model_dataset(self):
 		device = "cuda:0"
 		model_id = "gpt2-large"
 		model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
