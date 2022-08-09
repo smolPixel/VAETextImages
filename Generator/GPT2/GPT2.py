88170ad57df29a8360a1c8d6198ea14c0c2291e8
Frederic <bemeraude@hotmail.com>
2022-08-09 14:06:28 -0400

    auto commit

--- Generator/GPT2/GPT2.py
+++ Generator/GPT2/GPT2.py
@@ -60,7 +60,7 @@ class GPT2():
 			average_nll_gpt.append(outputs[0].detach())
 			print(logp.shape, target.shape)
 			fds
-			logp, target=self.datasets['train'].shape_for_loss_function(logp, target)
+			logp, target=self.datasets['train'].shape_for_loss_function(logp[:, :-1], target[:, 1:])
 			NLL_loss= self.loss_fn(logp, target)
 			loss = (NLL_loss) / batch_size
 			Average_loss.append(loss.item())
