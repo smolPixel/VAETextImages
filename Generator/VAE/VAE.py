53f36f46b4936a019d134589b72caf2cf2026c0f
Frederic <bemeraude@hotmail.com>
2022-08-09 09:52:09 -0400

    auto commit

--- Generator/VAE/VAE.py
+++ Generator/VAE/VAE.py
@@ -184,8 +184,8 @@ class VAE():
             self.epoch=epoch
             self.run_epoch()
         self.interpolate()
-        # self.generate_from_train()
-        self.create_graph()
+        self.generate_from_train()
+        # self.create_graph()
         # fds
 
 
