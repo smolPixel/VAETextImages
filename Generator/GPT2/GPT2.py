d7c09cb9b16591b01e14cfa919afa61fb9f0e35d
Frederic <bemeraude@hotmail.com>
2022-08-09 10:26:30 -0400

    auto commit

--- Generator/GPT2/GPT2.py
+++ Generator/GPT2/GPT2.py
@@ -43,7 +43,7 @@ class GPT2():
 		for iteration, batch in enumerate(data_loader):
 			encodings = self.tokenizer(batch['sentence'], return_tensors="pt", padding=True, truncation=True).to(self.device)
 			# Forward pass
-			outputs=self.model(encodings['input_ids'], labels=encondings['input_ids'].clone())
+			outputs=self.model(encodings['input_ids'], labels=encodings['input_ids'].clone())
 			logp, target=self.datasets['train'].shape_for_loss_function(logp, batch['target'])
 			NLL_loss, KL_loss= self.loss_fn(logp, target.to('cuda'),  mean, logv)
 
