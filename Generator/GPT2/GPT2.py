d47c39439787fbc472fd52150e4a545c20ed1cf7
Frederic <bemeraude@hotmail.com>
2022-08-09 10:24:34 -0400

    auto commit

--- Generator/GPT2/GPT2.py
+++ Generator/GPT2/GPT2.py
@@ -15,7 +15,7 @@ class GPT2():
 		self.loss_function_basic=train.loss_function
 
 	def init_model_dataset(self):
-		device = "cuda:0"
+		self.device = "cuda:0"
 		model_id = "gpt2-large"
 		model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
 		self.tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
@@ -41,12 +41,9 @@ class GPT2():
 		Average_NLL=[]
 		Average_KL_Div=[]
 		for iteration, batch in enumerate(data_loader):
-			print(batch)
-			fds
-			encodings = tokenizer(btext, return_tensors="pt", padding=True, truncation=True, max_length=128).to(device)
+			encodings = tokenizer(batch['sentence'], return_tensors="pt", padding=True, truncation=True).to(self.device)
 			# Forward pass
-			logp, mean, logv, z = self.model(batch)
-			batch_size = logp.shape[0]
+			outputs=self.model(encodings['input_ids'], labels=encondings['input_ids'].clone())
 			logp, target=self.datasets['train'].shape_for_loss_function(logp, batch['target'])
 			NLL_loss, KL_loss= self.loss_fn(logp, target.to('cuda'),  mean, logv)
 
