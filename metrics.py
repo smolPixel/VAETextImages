dbd55f39bdee05cb4ec15686132c806e226dfe0a
Frederic <bemeraude@hotmail.com>
2022-08-11 08:46:58 -0400

    auto commit

--- metrics.py
+++ metrics.py
@@ -1,5 +1,20 @@
 import math
+import torch
 
+def log_sum_exp(value, dim=None, keepdim=False):
+    """Numerically stable implementation of the operation
+    value.exp().sum(dim, keepdim).log()
+    """
+    if dim is not None:
+        m, _ = torch.max(value, dim=dim, keepdim=True)
+        value0 = value - m
+        if keepdim is False:
+            m = m.squeeze(dim)
+        return m + torch.log(torch.sum(torch.exp(value0), dim=dim, keepdim=keepdim))
+    else:
+        m = torch.max(value)
+        sum_exp = torch.sum(torch.exp(value - m))
+        return m + torch.log(sum_exp)
 
 def calc_mi(z, mu, logv):
 	"""Approximate the mutual information between x and z
@@ -25,15 +40,15 @@ def calc_mi(z, mu, logv):
 	var = logv.exp()
 
 	# (z_batch, x_batch, nz)
-	dev = z_samples - mu
+	dev = z - mu
 
 	# (z_batch, x_batch)
 	log_density = -0.5 * ((dev ** 2) / var).sum(dim=-1) - 0.5 * (
-			nz * math.log(2 * math.pi) + logvar.sum(-1)
+			nz * math.log(2 * math.pi) + logv.sum(-1)
 	)
 
 	# log q(z): aggregate posterior
 	# [z_batch]
-	log_qz = log_sum_exp(log_density, dim=1) - math.log(x_batch)
+	log_qz = log_sum_exp(log_density, dim=1) - math.log(bs)
 
 	return (neg_entropy - log_qz.mean(-1)).item()
