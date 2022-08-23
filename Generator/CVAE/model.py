1bd5239cebac2533050fbdc801cb7e45e7e85c2e
Frederic <bemeraude@hotmail.com>
2022-08-23 09:12:30 -0400

    auto commit

--- Generator/CVAE/model.py
+++ Generator/CVAE/model.py
@@ -25,11 +25,7 @@ class CVAE_model(nn.Module):
 
 		z = to_var(torch.randn([batch_size, self.argdict['latent_size']]))
 		z = z * std + mean
-		print(z.shape)
-		print(labels.shape)
 		z[:, :, -1]=labels
-		print(z)
-		fds
 		#last two dims
 		logp = self.decoder(input_sequence, z)
 
