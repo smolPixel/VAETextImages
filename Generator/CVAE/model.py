5abbb3a67888afdee8e1d75c9a911e5d15526adc
Frederic <bemeraude@hotmail.com>
2022-08-31 16:22:39 -0400

    auto commit

--- Generator/CVAE/model.py
+++ Generator/CVAE/model.py
@@ -39,32 +39,17 @@ class CVAE_model(nn.Module):
 
 	def encode(self, input_sequence, labels):
 		batch_size = input_sequence.size(0)
-		# sorted_lengths, sorted_idx = torch.sort(length, descending=True)
-		# input_sequence = input_sequence[sorted_idx]
 
-		# ENCODER
-		input_embedding = self.embedding(input_sequence)
-		# labels = labels.cuda()
-
-		_, hidden = self.encoder_rnn(input_embedding)
-
-		if self.bidirectional or self.num_layers > 1:
-			# flatten hidden state
-			hidden = hidden.view(batch_size, self.hidden_size * self.hidden_factor)
-		else:
-			hidden = hidden.squeeze()
-
-		# REPARAMETERIZATION
-		mean = self.hidden2mean(hidden)
-		logv = self.hidden2logv(hidden)
+		batch_size = input_sequence.size(0)
+		mean, logv = self.encoder(input_sequence)
 		std = torch.exp(0.5 * logv)
 
-		z = to_var(torch.randn([batch_size, self.latent_size]))
+		z = to_var(torch.randn([batch_size, self.argdict['latent_size']]))
 		z = z * std + mean
-		if len(z.shape)==3:
-			z[:, :, -1]=labels
-		elif len(z.shape)==2:
-			z[:, -1]=labels
+		if len(z.shape) == 3:
+			z[:, :, -1] = labels
+		elif len(z.shape) == 2:
+			z[:, -1] = labels
 		else:
 			raise ValueError()
 		return z
