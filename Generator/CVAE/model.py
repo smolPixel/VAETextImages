a650b250e82e16a032dc7a557675a74bf8e0dc3b
Frederic <bemeraude@hotmail.com>
2022-08-31 16:15:36 -0400

    auto commit

--- Generator/CVAE/model.py
+++ Generator/CVAE/model.py
@@ -69,77 +69,19 @@ class CVAE_model(nn.Module):
 			raise ValueError()
 		return z
 
-	def inference(self,  n=4, z=None, cat=0):
-		if z is None:
-			batch_size = n
-			z = to_var(torch.randn([batch_size, self.latent_size]))
+	def inference(self, z, labels):
+
+		print(z)
+		print(labels)
+		fds
+
+		if len(z.shape) == 3:
+			z[:, :, -1] = labels
+		elif len(z.shape) == 2:
+			z[:, -1] = labels
 		else:
-			batch_size = z.size(0)
+			raise ValueError()
 
-		if type(cat)==int:
-			# Convert to one hot and concat
-			ll = torch.zeros((batch_size, self.num_classes)).cuda()
-			ll[:, cat] = 1
-		elif len(cat.shape)==1:
-			ll = torch.zeros((batch_size, self.num_classes)).cuda()
-			for i, ind in enumerate(cat):
-				ll[i, ind]=1
-		else:
-			#If cat is already a tensor
-			ll=cat
+		generated = self.decoder.generate(z)  # , pad_idx=self.pad_idx, sos_idx=self.sos_idx)
 
-
-		z = torch.cat([z, ll], dim=1)
-		# print(z)
-
-		hidden = self.latent2hidden(z)
-
-		hidden = hidden.view(batch_size, self.hidden_factor, self.hidden_size)
-		hidden = torch.transpose(hidden, 0, 1)
-		hidden = hidden.contiguous()
-
-		# required for dynamic stopping of sentence generation
-		# sequence_idx = torch.arange(0, batch_size, out=self.tensor()).long()  # all idx of batch
-		# all idx of batch which are still generating
-		# sequence_running = torch.arange(0, batch_size, out=self.tensor()).long()
-		# sequence_mask = torch.ones(batch_size, out=self.tensor()).bool()
-		# idx of still generating sequences with respect to current loop
-		# running_seqs = torch.arange(0, batch_size, out=self.tensor()).long()
-
-		generations = self.tensor(batch_size, self.max_sequence_length).fill_(self.pad_idx).long()
-
-		t = 0
-		# input_sequence = input_sequence.unsqueeze(1)
-		while t < self.max_sequence_length:
-
-			if t == 0:
-				input_sequence = to_var(torch.Tensor(batch_size).fill_(self.sos_idx).long())
-				input_sequence = input_sequence.unsqueeze(1)
-
-			input_embedding = self.embedding(input_sequence)
-			if type(cat) == int:
-				# Convert to one hot and concat
-				ll = torch.zeros((input_embedding.shape[0], self.num_classes)).cuda()
-				ll[:, cat] = 1
-			elif len(cat.shape) == 1:
-				ll = torch.zeros((batch_size, self.num_classes)).cuda()
-				for i, ind in enumerate(cat):
-					ll[i, ind] = 1
-			else:
-				# If cat is already a tensor
-				ll = cat
-
-			# print(input_embedding.shape)
-			# print(ll.shape)
-
-			input_embedding = torch.cat([input_embedding, ll.unsqueeze(1)], dim=2)
-
-			output, hidden = self.decoder_rnn(input_embedding, hidden)
-
-			logits = self.outputs2vocab(output)
-
-			input_sequence = torch.argmax(logits, dim=-1)
-			generations[:, t]=input_sequence.squeeze(1)
-			t += 1
-
-		return generations, z
+		return generated, z
