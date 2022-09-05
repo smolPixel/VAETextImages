751920f00aa34fb8b0ed390b39b5ac7adb6620e1
Frederic <bemeraude@hotmail.com>
2022-09-05 14:08:49 -0400

    auto commit

--- Generator/Generator.py
+++ Generator/Generator.py
@@ -39,6 +39,9 @@ class generator():
         elif algo == "CVAE":
             from Generator.CVAE.CVAE import CVAE
             self.generator=CVAE(self.argdict, train, dev, test)
+        elif algo == "CVAE_Classic"
+            from Generator.CVAE_Classic.CVAE_Classic import CVAE_Classic
+            self.generator=CVAE_Classic(self.argdict, train, dev, test)
         elif algo == "BVAE":
             from Generator.BVAE.BVAE import BVAE
             self.generator = BVAE(self.argdict, train, dev, test)
