2a15c8e4bf3e272260a0708efa635fee643ce2dc
Frederic <bemeraude@hotmail.com>
2022-09-05 13:55:59 -0400

    auto commit

--- Generator/WSVAE/WSVAE.py
+++ Generator/WSVAE/WSVAE.py
@@ -227,10 +227,11 @@ class WSVAE():
                 #Why not optimize on the difference of mu and logv directly
                 # print("WARNING THIS SHOULD BE DONE AFTER GETTING ENCODER LOSS")
                 encoded_generated=self.model.encode(softmaxed_gumbeled)
+                z_normal_encoded= encoded_generated[:, :, :-2]
                 if len(z.shape)==3:
-                    encoded_generated[:, :, :-1]
+                    z_normal_encoded= encoded_generated[:, :, :-]
                 elif len(z.shape)==2:
-                    encoded_generated[:, :-1]
+                    z_normal_encoded= encoded_generated[:, :-2]
                 else:
                     raise ValueError
                 # print(z_normal_encoded.shape)
