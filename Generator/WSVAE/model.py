0f74ce547766c6ea2b8ba6e2bbd51fea2cd3dc8f
Frederic <bemeraude@hotmail.com>
2022-09-07 15:34:38 -0400

    auto commit

--- Generator/WSVAE/model.py
+++ Generator/WSVAE/model.py
@@ -100,46 +100,6 @@ class WSVAE_model(nn.Module):
             batch_size = z.size(0)
 
 
-        generated=self.decoder.generate(z)#, pad_idx=self.pad_idx, sos_idx=self.sos_idx)
-        #
-        # hidden = self.latent2hidden(z)
-        #
-        # # if self.bidirectional or self.num_layers > 1:
-        # #     # unflatten hidden state
-        # #     hidden = hidden.view(self.hidden_factor, batch_size, self.hidden_size)
-        # #     #Added the else here otherwise it was always unsqueezing which made it bug for bidir
-        # # else:
-        # #     hidden = hidden.unsqueeze(0)
-        # # if self.num_layers > 1:
-        #     # unflatten hidden state
-        # hidden=hidden.view(batch_size, self.hidden_factor, self.hidden_size)
-        # hidden=torch.transpose(hidden, 0, 1)
-        # hidden=hidden.contiguous()
-        #     # hidden = hidden.view(self.hidden_factor, batch_size, self.hidden_size)
-        #     #Added the else here otherwise it was always unsqueezing which made it bug for bidir
-        # # else:
-        # #     hidden = hidden.unsqueeze(0)
-        #
-        # generations = self.tensor(batch_size, self.max_sequence_length).fill_(self.pad_idx).long()
-        #
-        # t = 0
-        # while t < self.max_sequence_length:
-        #
-        #     if t == 0:
-        #         input_sequence = to_var(torch.Tensor(batch_size).fill_(self.sos_idx).long())
-        #         input_sequence = input_sequence.unsqueeze(1)
-        #
-        #     input_embedding = self.embedding(input_sequence)
-        #
-        #     output, hidden = self.decoder_rnn(input_embedding, hidden)
-        #
-        #     # output = self.outputs2embeds(output)
-        #
-        #     # logits = self.embed2vocab(output)
-        #     logits = self.outputs2vocab(output)
-        #
-        #     input_sequence = torch.argmax(logits, dim=-1)
-        #     generations[:, t]=input_sequence.squeeze(1)
-        #     t += 1
+        generated=self.decoder.generate(z, None)
 
         return generated, z
