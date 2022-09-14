1fdc8355866c7eab52835de470c8b855b71659e5
Frederic <bemeraude@hotmail.com>
2022-09-14 14:01:41 -0400

    auto commit

--- data/dataset.py
+++ data/dataset.py
@@ -7,11 +7,52 @@ from collections import defaultdict
 from torch.utils.data import Dataset
 import pandas as pd
 from nltk.tokenize import TweetTokenizer
-from process_data import *
+# from process_data import *
 from torchtext.vocab import build_vocab_from_iterator
 from torchvision import datasets, transforms
 import copy
+import math
 
+def get_dataFrame(argdict):
+    """Get the dataframe for the particular split. If it does not exist: create it"""
+    create_train=False
+    task=argdict['dataset']
+
+
+
+    # try:
+    #     dfTrain=pd.read_csv(f"{argdict['path']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t', index_col=0)
+    # except:
+    #     create_train=True
+    os.makedirs(f"{argdict['path']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/",exist_ok=True)
+
+    # if create_train:
+    print(f"{argdict['path']}/data/{task}/train.tsv")
+    dfTrain=pd.read_csv(f"{argdict['path']}/data/{task}/train.tsv", sep='\t')
+
+
+        # print(len(dfTrain))
+        # fds
+    dfVal=pd.read_csv(f'{argdict["path"]}/data/{task}/dev.tsv', sep='\t')
+    dfTest=pd.read_csv(f'{argdict["path"]}/data/{task}/test.tsv', sep='\t')
+    #
+    if argdict['dataset_size']!=0:
+    #     #Sampling balanced data
+    #     # print(len(dfTrain[dfTrain['label']==0]))
+    #     # prop=len(dfTrain[dfTrain['label']==0])/len(dfTrain)
+        dfTrain['true_label']=dfTrain['label']
+        nb_points=math.ceil(argdict['dataset_size']/len(argdict['categories']))
+        # print(prop)
+        # print(int(argdict['dataset_size']/len(argdict['categories'])))
+        # print(argdict['labelled_dataset_size'])
+        NewdfTrain=dfTrain[dfTrain['label']==0].sample(n=nb_points)
+        for i in range(1, len(argdict['categories'])):
+            # print(int(argdict['dataset_size']/len(argdict['categories'])))
+            # print(i)
+            NewdfTrain=pd.concat([NewdfTrain ,dfTrain[dfTrain['label']==i].sample(n=nb_points)])
+        dfTrain.drop(NewdfTrain.index)
+        dfTrain.to_csv(f"{argdict['path']}/SelectedData/{argdict['dataset']}/{argdict['dataset_size']}/train_{argdict['random_seed']}.tsv", sep='\t')
+    return dfTrain, dfVal, dfTest
 
 def create_datasets(argdict):
     if argdict['dataset'] in ['SST2', "SST100"]:
