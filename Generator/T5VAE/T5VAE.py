3de3b640282d5295f6e418bcf2b609f926f45a94
Frederic <bemeraude@hotmail.com>
2022-09-15 10:51:48 -0400

    auto commit

--- Generator/T5VAE/T5VAE.py
+++ Generator/T5VAE/T5VAE.py
@@ -162,8 +162,7 @@ class T5VAE(LightningModule):
 		return
 
 	def validation_step(self, batch, batch_idx):
-		print(batch)
-		tokenized= self.tokenizer(batch[self.field_input], padding=True, truncation=True)
+		tokenized= self.tokenizer(batch['sentence'], padding=True, truncation=True)
 		recon_loss, reg_loss = self.run_batch(tokenized, batch_idx)
 		loss = recon_loss + reg_loss
 		# mi = calc_batch_mi(self, batch)
