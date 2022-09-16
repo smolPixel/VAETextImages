a5517154373886934f01c8e86bf22257980f9654
Frederic <bemeraude@hotmail.com>
2022-09-16 15:13:53 -0400

    auto commit

--- Generator/T5VAE/model.py
+++ Generator/T5VAE/model.py
@@ -77,13 +77,8 @@ class ModifiedT5ForConditionalGeneration(T5ForConditionalGeneration):
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
             )
-            print('----------------------------')
-            print(encoder_outputs.hidden_states[0])
             pooled = self.pool(encoder_outputs.hidden_states)
-            print(pooled[0])
             z, mu, logvar = self.calculate_latent(pooled)
-            print(z)
-            print('00000000000000000000000000000000')
         elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
             encoder_outputs = BaseModelOutput(
                 last_hidden_state=encoder_outputs[0],
@@ -259,6 +254,10 @@ class ModifiedT5ForConditionalGeneration(T5ForConditionalGeneration):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
+
+        print(encoder_outputs)
+        print(self.encoder(input_ids))
+        fds
 
         # If the user passed a tuple for encoder_outputs, we wrap it in a BaseModelOutput when return_dict=True
 
