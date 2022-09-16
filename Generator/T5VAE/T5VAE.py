8bc1d25da878efd5cadeeb938c5457c46144f465
Frederic <bemeraude@hotmail.com>
2022-09-16 10:37:55 -0400

    auto commit

--- Generator/T5VAE/T5VAE.py
+++ Generator/T5VAE/T5VAE.py
@@ -256,12 +256,12 @@ class T5VAE(LightningModule):
 
 		trainer = pl.Trainer(
 			callbacks=[early_stop_callback, checkpoint_callback],
-			max_epochs=15
+			max_epochs= 1 #15
 		)
 
 		train_loader = DataLoader(
 			dataset=self.datasets['train'],
-			batch_size=64,  # self.argdict.batch_size,
+			batch_size=256,  # self.argdict.batch_size,
 			shuffle=True,
 		)
 
