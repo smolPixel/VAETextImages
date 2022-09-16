2cfb6fa4c5ec8ab3a3a997fbed5f5398ddc842ad
Frederic <bemeraude@hotmail.com>
2022-09-16 15:15:07 -0400

    auto commit

--- Generator/T5VAE/model.py
+++ Generator/T5VAE/model.py
@@ -255,10 +255,6 @@ class ModifiedT5ForConditionalGeneration(T5ForConditionalGeneration):
             return_dict=return_dict,
         )
 
-        print(encoder_outputs[0])
-        print(self.encoder(input_ids)[0])
-        fds
-
         # If the user passed a tuple for encoder_outputs, we wrap it in a BaseModelOutput when return_dict=True
 
         return encoder_outputs
