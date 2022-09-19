0f3aa940e24fcfe54cf9cea6c1cfcf221dab7a1c
Frederic <bemeraude@hotmail.com>
2022-09-19 09:53:38 -0400

    auto commit

--- Generator/T5VAE/model.py
+++ Generator/T5VAE/model.py
@@ -77,6 +77,7 @@ class ModifiedT5ForConditionalGeneration(T5ForConditionalGeneration):
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
             )
+            print(encoder_outputs.hidden_states)
             pooled = self.pool(encoder_outputs.hidden_states)
             z, mu, logvar = self.calculate_latent(pooled)
         elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
