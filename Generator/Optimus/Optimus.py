0aa68082844ec800d07f6490f5a9ba3be2aade10
Frederic <bemeraude@hotmail.com>
2022-09-25 15:02:51 -0400

    auto commit

--- Generator/Optimus/Optimus.py
+++ Generator/Optimus/Optimus.py
@@ -1,3 +1,10 @@
+from pytorch_transformers import (WEIGHTS_NAME, AdamW, WarmupLinearSchedule,
+                                  BertConfig, BertForLatentConnector, BertTokenizer,
+                                  GPT2Config, GPT2ForLatentConnector, GPT2Tokenizer,
+                                  OpenAIGPTConfig, OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
+                                  RobertaConfig, RobertaForMaskedLM, RobertaTokenizer)
+
+
 class OptimusVAE():
 	def __init__(self, argdict, train, dev, test):
 		gfd
