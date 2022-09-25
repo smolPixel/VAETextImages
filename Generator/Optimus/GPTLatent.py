a99c24ed1edfe5eef74bf90ab5735837099862dd
Frederic <bemeraude@hotmail.com>
2022-09-25 16:48:12 -0400

    auto commit

--- Generator/Optimus/GPTLatent.py
+++ Generator/Optimus/GPTLatent.py
@@ -54,7 +54,7 @@ class GPT2ForLatentConnector():
 		# self.init_weights()
 		# self.tie_weights()
 
-
+		pass
 		# self.latent_as_gpt_emb = latent_as_gpt_emb
 		# self.latent_as_gpt_memory = latent_as_gpt_memory
 
