ce190d58c68d5516a7563335fa4baf3a11093bd3
Frederic <bemeraude@hotmail.com>
2022-09-24 20:01:24 -0400

    auto commit

--- Generator/T5VAE/T5VAE.py
+++ Generator/T5VAE/T5VAE.py
@@ -268,6 +268,7 @@ class T5VAE(LightningModule):
 		return loss
 
 	def regularization_loss(self, mu, logvar, training=False):
+		return 0
 		dimensionwise_loss = -0.5 * (1 + logvar - mu ** 2 - logvar.exp())
 		#KL thresholding, 2.2.4
 		if self.min_z and training:
