5857737bfb374b232e1e4283d9ee13c9ba60ccf9
Frederic <bemeraude@hotmail.com>
2022-09-28 16:20:33 -0400

    auto commit

--- Generator/OptimusHomemade/model.py
+++ Generator/OptimusHomemade/model.py
@@ -17,7 +17,7 @@ class OptimusHomemade(nn.Module):
 	def forward(self, batch):
 		print(batch)
 		sents=batch['sentence']
-		encoded=self.encoder_tokenizer(sents, padding=True, truncation=True)
+		encoded=self.encoder_tokenizer(sents, padding=True, truncation=True, return_tensors='pt')
 		print(encoded)
 		output=self.encoder(input_ids=encoded['input_ids'], attention_mask=encoded['attention_mask'])
 		print(output)
