7fc8e3e58bd0d3b1ccfc4b2a3f3e597252a9975c
Frederic <bemeraude@hotmail.com>
2022-10-02 11:03:40 -0400

    auto commit

--- Generator/OptimusHomemade/GPT2Latent.py
+++ Generator/OptimusHomemade/GPT2Latent.py
@@ -98,7 +98,7 @@ class GPT2ModelLatent(GPT2PreTrainedModel):
 	def set_output_embeddings(self, new_embeddings):
 		self.lm_head = new_embeddings
 
-	def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
+	def prepare_inputs_for_generation(self, input_ids, z, past=None, **kwargs):
 		token_type_ids = kwargs.get("token_type_ids", None)
 		# only last token for inputs_ids if past is defined in kwargs
 		if past:
@@ -119,6 +119,7 @@ class GPT2ModelLatent(GPT2PreTrainedModel):
 			position_ids = None
 		return {
 			"input_ids": input_ids,
+			"z": z,
 			"past_key_values": past,
 			"use_cache": kwargs.get("use_cache"),
 			"position_ids": position_ids,
@@ -337,7 +338,7 @@ class GPT2ModelLatent(GPT2PreTrainedModel):
 		)
 
 
-	def generate(self, input_seq, z):
+	def generate(self, input_ids, z):
 		logits_processor = LogitsProcessorList()
 		while True:
 			# prepare model inputs
