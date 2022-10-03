abfa85c3041d4a43d0841eeae1090842f8a7a9ea
Frederic <bemeraude@hotmail.com>
2022-10-03 11:26:22 -0400

    auto commit

--- Generator/OptimusHomemade/Optimus.py
+++ Generator/OptimusHomemade/Optimus.py
@@ -42,11 +42,9 @@ class OptimusVAE():
 	def loss_fn(self, logp, target, mean, logv, anneal_function, step, k, x0, lamb):
 		NLL_loss = self.loss_function_basic(logp, target)
 		# KL Divergence
-		print(logv.shape)
 		dimensionwise_loss = -0.5 * (1 + logv - mean ** 2 - logv.exp())
 		dimensionwise_loss[dimensionwise_loss < lamb] = lamb
-		KL_loss = dimensionwise_loss.sum(-1)
-		print(KL_loss)
+		KL_loss = dimensionwise_loss.sum()
 		# KL_loss = -0.5 * torch.sum(1 + logv - mean.pow(2) - logv.exp())
 		KL_weight = self.kl_anneal_function(anneal_function, step, k, x0)
 
