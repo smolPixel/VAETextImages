a4f03d5cfc062987a859b7964090885dba1d3e6a
frederic <bemeraude@hotmail.com>
2023-06-20 09:26:30 -0400

    m

--- Generator/SeqGAN/seqgan_instructor.py
+++ Generator/SeqGAN/seqgan_instructor.py
@@ -18,6 +18,7 @@ import torch.nn as nn
 
 # import config as cfg
 from Generator.SeqGAN.utils import Signal
+from Decoders.LSTMGAN import SeqGAN_G
 # from metrics.bleu import BLEU
 # from metrics.clas_acc import ACC
 # from metrics.nll import NLL
