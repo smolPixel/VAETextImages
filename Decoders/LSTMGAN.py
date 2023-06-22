a919f09724bddde74ee21fd65af962c7077d881f
frederic <bemeraude@hotmail.com>
2023-06-22 12:51:30 -0400

    m

--- Decoders/LSTMGAN.py
+++ Decoders/LSTMGAN.py
@@ -33,9 +33,6 @@ class LSTMGenerator(nn.Module):
         :param hidden: (h, c)
         :param need_hidden: if return hidden, use for sampling
         """
-        print(inp)
-        print(inp.shape)
-        fds
         emb = self.embeddings(inp)  # batch_size * len * embedding_dim
         if len(inp.size()) == 1:
             emb = emb.unsqueeze(1)  # batch_size * 1 * embedding_dim
